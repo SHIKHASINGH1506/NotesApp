87d25d19408d0a07c1d11fd2f63c451b4316eb9b
Shikha <shikhakumarisingh9715@gmail.com>
2022-03-30 13:21:27 +0530

    imported mockman

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { Routes, Route } from "react-router-dom";
 import { LandingPage } from './Pages';
 import { Home} from "./Pages/index";
 import { Navbar } from "./component";
+import Mockman from 'mockman-js';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path='/' element={<LandingPage />}/>
-        <Route path='/home' element={<Home />}/>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/mockman' element={<Mockman />} />
+        <Route path='/home' element={<Home />} />
       </Routes>
     </div>
   );
