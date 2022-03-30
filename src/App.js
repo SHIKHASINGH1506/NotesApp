418ffe58c3ba4de716a2e9e6ced382a7f9479f17
Shikha <shikhakumarisingh9715@gmail.com>
2022-03-30 12:55:21 +0530

    modified route

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { LandingPage } from './Pages';
 import { Home} from "./Pages/index";
-import { Navbar } from "./component/index";
+import { Navbar } from "./component";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path='/' element={<LandingPage />}></Route>
-        <Route path='/home' element={<Home />}></Route>
+        <Route path='/' element={<LandingPage />}/>
+        <Route path='/home' element={<Home />}/>
       </Routes>
     </div>
   );
