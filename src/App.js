948e7af347a3899598049c7cd9656126c54ef9ad
Shikha <shikhakumarisingh9715@gmail.com>
2022-03-31 23:46:46 +0530

    removed mockman stylesheet and mockman imports

--- src/App.js
+++ src/App.js
@@ -2,33 +2,57 @@ import "./App.css";
 import 'react-toastify/dist/ReactToastify.css';
 
 import { Routes, Route } from "react-router-dom";
-import {ToastContainer} from 'react-toastify';
-import Mockman from 'mockman-js';
+import { ToastContainer } from 'react-toastify';
+// import Mockman from 'mockman-js';
 
 import { LandingPage, Home, Login, Signup } from './Pages';
 import { Navbar } from "./component";
+import {useNote} from "context";
+import {addNote} from 'service/note-service';
+import {useToast} from 'custom-hooks/useToast';
 
 function App() {
+  const {noteData, setNoteData, buttonFocus, setButtonFocus, dispatch} = useNote();
+  const {showToast} = useToast();
+  const initialNote = {
+    title: '',
+    body: ''
+  };
+
+  const onPageClickHandler = (e) => {
+    e.stopPropagation();
+    if(!(noteData.title.trim() === '' && noteData.body.trim() === '')) {
+      addNote(dispatch, {note: noteData}, showToast);
+      setTimeout(() => {
+        setNoteData(initialNote);
+        setButtonFocus(false);
+      }, 1000)
+    }
+    setNoteData(initialNote);
+    buttonFocus ? setButtonFocus(false) : "";
+  }
   return (
     <div className="App">
-       <ToastContainer 
-        position="top-right"
-        autoClose={2000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        draggable
-        pauseOnHover
-      />
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<LandingPage />} />
-        <Route path='/mockman' element={<Mockman />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
-      </Routes>
+      <div onClick={(e) => onPageClickHandler(e)}>
+        <ToastContainer
+          position="top-right"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          draggable
+          pauseOnHover
+        />
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+          {/* <Route path='/mockman' element={<Mockman />} /> */}
+          <Route path='/home' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+      </div>
     </div>
   );
 }
