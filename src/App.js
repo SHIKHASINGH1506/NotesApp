60b3914874be4e8701ec2b81a3dd42da232be358
Shikha <shikhakumarisingh9715@gmail.com>
2022-04-02 10:59:06 +0530

    refactored the code

--- src/App.js
+++ src/App.js
@@ -1,60 +1,13 @@
 import "./App.css";
 import 'react-toastify/dist/ReactToastify.css';
-
-import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 // import Mockman from 'mockman-js';
-
-import { LandingPage, Home, Login, Signup } from './Pages';
+import { Routes } from "./route/AppRoute";
 import { Navbar } from "./component";
-import {useNote} from "context";
-import {addNote} from 'service/note-service';
-import {useToast} from 'custom-hooks/useToast';
 
 function App() {
-  const {
-    noteData, 
-    setNoteData, 
-    dispatch,
-    state : {
-      addFormFocus
-    }
-  } = useNote();
-  const {showToast} = useToast();
-  const initialNote = {
-    title: '',
-    body: ''
-  };
-
-  const onPageClickHandler = (e) => {
-    e.stopPropagation();
-    if(!(noteData.title.trim() === '' && noteData.body.trim() === '')) {
-      addNote(dispatch, {note: noteData}, showToast);
-      setTimeout(() => {
-        setNoteData(initialNote);
-        dispatch({
-          type: 'SET_NEW_NOTE_FOCUS',
-          payload: {
-            addFormFocus: false,
-            editFormFocus: false
-          }
-        })
-      }, 1000)
-    }
-    setNoteData(initialNote);
-   addFormFocus 
-    ? dispatch({
-      type: 'SET_NEW_NOTE_FOCUS',
-      payload: {
-        addFormFocus: false,
-        editFormFocus: false
-      }
-      })
-    : "";
-  }
   return (
     <div className="App">
-      <div onClick={(e) => onPageClickHandler(e)}>
         <ToastContainer
           position="top-right"
           autoClose={2000}
@@ -66,14 +19,7 @@ function App() {
           pauseOnHover
         />
         <Navbar />
-        <Routes>
-          <Route path='/' element={<LandingPage />} />
-          {/* <Route path='/mockman' element={<Mockman />} /> */}
-          <Route path='/home' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<Signup />} />
-        </Routes>
-      </div>
+        <Routes />
     </div>
   );
 }
