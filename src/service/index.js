c248fd240ca630db64f3324500859aa8cb76acda
Shikha <shikhakumarisingh9715@gmail.com>
2022-04-03 13:26:21 +0530

    import export

--- src/service/index.js
+++ src/service/index.js
@@ -1,4 +1,6 @@
 import { loginUser, signupUser } from './auth-service'
 import { getNotes, addNote, editNote, deleteNote } from './note-service'
+import { archiveNote, unArchiveNote } from './archive-service';
 
-export { loginUser, signupUser, getNotes, addNote, editNote, deleteNote };
+
+export { loginUser, signupUser, getNotes, addNote, editNote, deleteNote, archiveNote, unArchiveNote };
