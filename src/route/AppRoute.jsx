4cd649f782a014344499b6ca4d6d55cb166b4b71
Shikha <shikhakumarisingh9715@gmail.com>
2022-04-07 00:49:48 +0530

    added protected rouets

--- src/route/AppRoute.jsx
+++ src/route/AppRoute.jsx
@@ -1,15 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import { LandingPage, Home, Login, Signup, Trash, Archive } from "Pages";
+import { ProtectedRoute } from "./ProtectedRoute";
 
 const AppRoute = () => {
   return (
     <Routes>
-      <Route path='/' element={<LandingPage />} />
-      <Route path='/home' element={<Home />} />
       <Route path='/login' element={<Login />} />
       <Route path='/signup' element={<Signup />} />
-      <Route path='/trash' element={<Trash />} />
-      <Route path='/archive' element={<Archive />} />
+      <Route path='/' element={<LandingPage />} />
+      <Route path='/' element={<ProtectedRoute />} >
+        <Route path='/home' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/trash' element={<Trash />} />
+        <Route path='/archive' element={<Archive />} />
+      </Route>
     </Routes>
   );
 }
