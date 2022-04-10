eced81ce85a00e6d69f73924c97d0d088ed84bdc
Shikha <shikhakumarisingh9715@gmail.com>
2022-04-10 10:53:52 +0530

    feat : sort by priority

--- src/component/filter-modal/FilterModal.jsx
+++ src/component/filter-modal/FilterModal.jsx
@@ -2,9 +2,17 @@ import './filter-modal.css';
 import CloseIcon from '@mui/icons-material/Close';
 import { v4 as uuid } from "uuid";
 import {useSortFilter, useNote} from 'context';
+import {PriorityBox} from 'component';
 
 export const FilterModal = ({filterFocusHandler}) => {
-  const {sortFilterState:{sortBy, filterBylabels}, sortFilterDispatch } = useSortFilter();
+  const {
+    sortFilterState:{
+      sortBy, 
+      filterBylabels,
+      sortByPriority
+    }, 
+    sortFilterDispatch 
+  } = useSortFilter();
   const {state:{labels}} = useNote();
   const sortOptions = [
     {
@@ -20,6 +28,16 @@ export const FilterModal = ({filterFocusHandler}) => {
       value: 'date created: earliest'
     }
   ]
+  const dropdownOption=[
+    {
+      id: 1,
+      value: 'High to Low'
+    },
+    {
+      id: 2,
+      value: 'Low to High'
+    }
+  ];
   const filterByLabelsHandler = labelId => {
     const newFilterByLabels = filterBylabels.map(
       filter => filter.id === labelId 
@@ -31,7 +49,13 @@ export const FilterModal = ({filterFocusHandler}) => {
       type: "FILTER_BY_LABELS", 
       payload: { filterBylabels: newFilterByLabels }
     });
-}
+  }
+  const setPriorityHandler = e => {
+    sortFilterDispatch({
+      type: "SORT_BY_PRIORITY", 
+      payload: { sortByPriority: e.target.value }
+    });
+  }
   return(
     <div className="filter-sort-wrapper py-2 px-4">
       <div className="card__title-wrapper d-flex items-center justify-between">
@@ -40,35 +64,36 @@ export const FilterModal = ({filterFocusHandler}) => {
       </div>
       <div className="sort-option-wrapper">
         <div className="sort-wrapper d-flex flex-col">
-          <label htmlFor="sort">Sort by: </label>
-          <select
-            name="sort"
-            id="sort"
-           // className
+          <div className="label-card-list d-flex w-full">
+            <label htmlFor="sort">Sort by: </label>
+            <select
+              name="sort"
+              id="sort"
+              className="p-2 w-full"
               value={sortBy}
               onChange={(e) => sortFilterDispatch({
                 type:'SORT_BY',
                 payload:{ sortBy: e.target.value}
               })}
-          >
-            {sortOptions.map( (option, index) => {
-           return index===0
-              ? (
-                <option value="" disabled selected>
-                 {option.value}
-                </option>
-                )
-              : (<option key={option.id} value={option.value}>{option.value}</option>)
-              })}
-          </select>
-        </div>
+            >
+              {sortOptions.map( (option, index) => {
+            return index===0
+                ? (
+                  <option value="" disabled selected>
+                  {option.value}
+                  </option>
+                  )
+                : (<option key={option.id} value={option.value}>{option.value}</option>)
+                })}
+            </select>
+          </div>
         {labels.length>0 && <div className="filter-wrapper d-flex flex-col">
           <p>Filter by: </p>
           <div className="label-card-list d-flex">
             {labels.map(({id, label}) => (
-              <label key={id} htmlFor={label} className="list-item">
+              <label key={id} htmlFor={label} className="d-flex justify-between items-center">
                 <input
-                  className="mr-4"
+                  className="mr-2"
                   type="checkbox"
                   id={label}
                   name={label}
@@ -80,20 +105,41 @@ export const FilterModal = ({filterFocusHandler}) => {
               ))}
           </div>
         </div>}
+        {/* <p>Sort By Priority</p> */}
+        <div className="label-card-list d-flex w-full">
+          <label htmlFor="sort">Sort by Priority: </label>
+            <select 
+            className="p-2 w-full"
+            name="priority" 
+            id=""
+            value={sortByPriority}
+            onChange={e => setPriorityHandler(e)}>
+            {dropdownOption.map(option => (
+              <option
+                key={option.id}
+                value={option.value}
+                >
+                {option.value}
+              </option>
+            ))}
+            </select>
+        </div>
         <div className="d-flex clear-filter-btn my-2">
           <button className="bttn bttn-primary bttn-sm" 
             onClick={() => sortFilterDispatch({
               type:'CLEAR_FILTER',
               payload: {
                 sortBy: '',
+                sortByPriority: '',
                 filterBylabels: filterBylabels.map(
                   label=> ({...label, filtered: false})
                 )
               }
             })}
           >
-              Clear
+            Clear
           </button>
+        </div>
         </div>
       </div>
     </div>
