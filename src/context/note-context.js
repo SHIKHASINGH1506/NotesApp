ca9ab453b867b2101606d1c5f3de78789f2ea26d
Shikha <shikhakumarisingh9715@gmail.com>
2022-04-10 10:56:30 +0530

    get archives and trash note api called on first render

--- src/context/note-context.js
+++ src/context/note-context.js
@@ -1,6 +1,6 @@
 import { useState, createContext, useContext, useReducer, useEffect } from 'react';
 import { notesReducer, initialNoteState } from 'reducer';
-import { getNotes } from 'service';
+import { getNotes, getNotesFromTrash, getAllArchiveNotes } from 'service';
 
 const NoteContext = createContext();
 const useNote = () => useContext(NoteContext);
@@ -9,7 +9,7 @@ const NoteProvider = ({ children }) => {
   const initialNote = {
     title: '',
     body: '',
-    priority: ''
+    priority: 'None'
   };
   const [noteData, setNoteData] = useState(initialNote);
   const [editNoteData, setEditNoteData] = useState();
@@ -19,26 +19,83 @@ const NoteProvider = ({ children }) => {
   const getAllNotes = () => {
     getNotes(dispatch);
   }
+  const getArchiveNotes = async () => {
+    try {
+      const { data: { archives } } = await getAllArchiveNotes();
+      dispatch({type: 'INIT_ARCHIVES_SUCCESS', 
+        payload: {
+          archives,
+          loading: false,
+          error: null,
+          addFormFocus: false,
+          editFormFocus: false,
+      }});
+    } catch (error) {
+      dispatch({type: 'INIT_NOTES_ERROR', 
+        payload: {
+          archives,
+          loading: false,
+          error: "Error while fetching archive notes",
+          addFormFocus: false,
+          editFormFocus: false,
+      }});
+      console.log(error.response.data);
+    }
+  }
+
+  const getAlltrashedNotes = async () => {
+    try {
+      const { data :{trash} } = await getNotesFromTrash();
+        dispatch({
+          type: 'INIT_TRASH_SUCCESS', 
+          payload: {
+            trash,
+            loading: false,
+            error: null,
+            addFormFocus: false,
+            editFormFocus: false
+          }});
+    } catch (error) {
+      dispatch({
+        type: 'INIT_NOTES_ERROR',
+        payload: {
+          loading: false,
+          error: 'Error loading notes in trash',
+          addFormFocus: false,
+          editFormFocus: false
+        }
+      })
+    }
+  }
 
   useEffect(() => {
     const token = JSON.parse(localStorage.getItem("token"));
     token && getAllNotes();
   }, []);
 
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem("token"));
+    token && getArchiveNotes();
+  }, []);
+
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem("token"));
+    token && getAlltrashedNotes();
+  }, []);
   return (
     <NoteContext.Provider value={{
-      noteData, 
-      setNoteData, 
-      editNoteData,  
-      setEditNoteData, 
+      noteData,
+      setNoteData,
+      editNoteData,
+      setEditNoteData,
       archiveNoteData,
       setArchiveNoteData,
-      state, 
+      state,
       dispatch
     }}>
-      { children }
+      {children}
     </NoteContext.Provider>
   );
 }
 
-export {NoteProvider, useNote}
+export { NoteProvider, useNote }
